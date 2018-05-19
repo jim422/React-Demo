e009911eade3ccccacff5c60bd8732501a15ee3d
jinfengjie <jinfengjie@weiboyi.com>
2018-05-19 16:45:28 +0800

    方格游戏

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Game from './component/Game.js';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +8,12 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">first Demo</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Game
         </p>
+        <Game></Game>
       </div>
     );
   }
